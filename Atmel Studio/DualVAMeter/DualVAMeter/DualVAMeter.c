a7b8907189cca251566210386f3121be49cf51ec
ryood <o.d.r.ryo@gmail.com>
2014-10-19 13:12:49 +0900

    DualVAMeter.c 修正 SPLC792-I2C-2　ブレッドボード図修正

--- Atmel Studio/DualVAMeter/DualVAMeter/DualVAMeter.c
+++ Atmel Studio/DualVAMeter/DualVAMeter/DualVAMeter.c
@@ -13,7 +13,7 @@
 ポート対応	[ATmega88]	x:存在しない(PC6はRESET) -:空き
 bit		7	6	5	4	3	2	1	0
 portB	-	-	-	-	-	-	-	CKO		PB0:システムクロック出力
-portC	x	x	SCL	SDA	A-M	V-M	A+M	V+M		I2C(PC5:クロック PC4:データ) ADC(PC3 ~ 0 電流電圧計)
+portC	x	x	SCL	SDA	A-M	V-M	A+M	V+M		I2C(PC5:クロック PC4:データ) ADC(PC3..0 電流電圧計)
 portD	-	-	-	-	-	RST	-	-		LCD		
 
 【ヒューズビット】 8MHzクロック システムクロック出力
@@ -32,12 +32,34 @@ ATmega328P	8MHz（内蔵RC）
 #include <avr/interrupt.h>
 #include <avr/signal.h>
 #include <compat/twi.h>
+#include <avr/sleep.h>
+
+//Vref(V)
+#define	F_VREF	2.78	//シャントレギュレータ
+
+//電流測定用のシャント抵抗(Ω)
+#define	F_SHUNT_R	9.9
+
+//電圧測定用の分圧抵抗(kΩ)
+#define	F_VMETER_RA	 99.5	//ADC入力点からGND側
+#define	F_VMETER_RB	298.0	//ADC入力点から回路側
+
+//バッファサイズ
+//total()の結果がオーバーフローしないよう60以下であること（10で十分）
+#define	BUFSIZE	10
+
+//表示位置
+#define	VOLTAGE_X	0
+#define	VOLTAGE_Y	0
+#define	CURRENT_X	0
+#define	CURRENT_Y	1
 
 // コントラストの設定
 unsigned char contrast = 0b110000;	// 3.0V時 数値を上げると濃くなります。
 									// 2.7Vでは0b111000くらいにしてください。。
 									// コントラストは電源電圧，温度によりかなり変化します。実際の液晶をみて調整してください。
 										
+
 // この関数でμ秒のウェイトを入れます。引数 100 = 100μ秒ウェイト
 // クロックスピードにより調整してください。
 static void wait_us(short t)
@@ -56,7 +78,16 @@ static void wait_ms(short t)
 	}
 }
 
+static void wait_sec(uint8_t sec)
+{
+	uint8_t	i, m;
+	for (i = 0; i < sec; i++)
+	for (m = 0; m < 5; m++)
+	wait_ms(200);
+}
+
 /*/////////////////////////////////////////////////////////////////////////////
+ *
  * I2C LCD
  *
  */
@@ -173,8 +204,14 @@ void i2c_puts(unsigned char *s)
 	}
 }
 
+/*/////////////////////////////////////////////////////////////////////////////
+ *
+ * LCD
+ *
+ */
+
 // LCDの初期化
-void init_lcd()
+void lcdInit()
 {
 	wait_ms(40);
 	i2c_cmd(0b00111000); // function set
@@ -193,13 +230,137 @@ void init_lcd()
 	wait_ms(2);			 // Clear Displayは追加ウェイトが必要
 }
 
+#define lcdSetPos(x,y)	i2c_cmd(0b10000000+40*(y)+x)
+#define lcdPutChar(c)	i2c_data(c)
+#define lcdPutStr(s)	i2c_puts(s)
+
+static	uint8_t	kbuf[5] = {0};	//16bit整数5桁分	lcdPutUInt()で使用
+	
+void	lcdPutUInt(uint16_t n)
+{
+	uint8_t	i = 0;
+	
+	do
+	{
+		kbuf[i++] = n % 10;
+		n /= 10;
+	}
+	while (0 < n);
+	
+	while (i != 0)
+	lcdPutChar('0' + kbuf[--i]);
+}
+
+/*/////////////////////////////////////////////////////////////////////////////
+ *
+ * 電圧・電流の表示
+ *
+ */
+
+//電圧を表示する
+//引数：	mV単位の値
+void	lcdPutVoltage(uint16_t mValue, int pos)
+{
+	uint16_t	num, dec;
+
+	lcdSetPos(VOLTAGE_X + pos*8, VOLTAGE_Y);	
+	if (pos == 1)
+		lcdPutChar('-');
+	
+	num = mValue / 1000;	//整数部
+	dec = mValue % 1000;	//小数部
+	
+	//10mV未満は小数第2位まで表示（小数第3位以下は切り捨て）
+	//10mV以上は小数第1位まで表示（小数第2位以下は切り捨て）
+	//[9.99][10.0]mVが境目
+	if (num < 10)
+	{
+		lcdPutChar(' ');
+		lcdPutUInt(num);
+		lcdPutChar('.');
+		//小数部3桁中2桁表示（3桁目は切り捨て）
+		dec /= 10;
+		if (dec < 10) lcdPutChar('0');
+			lcdPutUInt(dec);
+	}
+	else
+	{
+		lcdPutUInt(num);
+		lcdPutChar('.');
+		//小数部3桁中1桁表示（2桁目以降は切り捨て）
+		lcdPutUInt(dec / 1000);
+	}
+	
+	lcdPutStr("V");
+}
+
+//電流を表示する
+//引数：	0.1mA単位の値
+void	lcdPutCurrent(uint16_t mValue, int pos)
+{
+	uint16_t	num, dec;
+	
+	lcdSetPos(CURRENT_X + pos*8, CURRENT_Y);
+	if (pos == 1)
+		lcdPutChar('-');
+	
+	num = mValue / 10;	//整数部
+	dec = mValue % 10;	//小数部
+	
+	//100mA未満は小数第1位まで表示
+	//100mA以上は整数部のみ表示（小数点以下は切り捨て）
+	//[99.9][100]mAが境目
+	if (num < 100)
+	{
+		if (num < 10) lcdPutChar(' ');
+			lcdPutUInt(num);
+		lcdPutChar('.');
+		lcdPutUInt(dec);
+	}
+	else
+	{
+		if (num < 1000) lcdPutChar(' ');
+			lcdPutUInt(num);
+	}
+	
+	lcdPutStr("mA");
+}
+
+//バッファの値を合計する
+uint16_t	total(uint16_t values[])
+{
+	uint8_t	i;
+	uint16_t	n = 0UL;
+	for (i = 0; i < BUFSIZE; i++) n += values[i];
+	return n;
+}
+
+//ADMUXに設定する基準電圧と入力ピンの内容
+//REFS0 -> 00:AREF, 01:AVCC, 11:内部基準1.1V
+//MUX0  -> 0111-0000:PC7-PC0, 1110:内部的に1.1Vを接続, 1111:内部的に0Vを接続
+#define	ADC_SET_PVOLTAGE	((0b01 << REFS0) | (0b0000 << MUX0))
+#define	ADC_SET_PCURRENT	((0b01 << REFS0) | (0b0001 << MUX0))
+#define	ADC_SET_NVOLTAGE	((0b01 << REFS0) | (0b0010 << MUX0))
+#define	ADC_SET_NCURRENT	((0b01 << REFS0) | (0b0011 << MUX0))
 
 int main(void)
 {
+	uint16_t	adcPVoltages[BUFSIZE] = {0UL};
+	uint16_t	adcPCurrents[BUFSIZE] = {0UL};
+	uint16_t	adcNVoltages[BUFSIZE] = {0UL};
+	uint16_t	adcNCurrents[BUFSIZE] = {0UL};
+	uint16_t	adcValue;
+	uint16_t	mVoltage, mCurrent;
+	uint8_t	idx;
+	uint8_t	idx2;
+	float	fv, fa;
+		
 	// ポートの初期化
 	DDRD = _BV(2);			 // RSTピンを出力に
 	PORTC = _BV(4) | _BV(5); // SCL, SDA内蔵プルアップを有効
 	
+	
+	// デバイスの初期化
 	i2c_init();				 // AVR内蔵I2Cモジュールの初期化
 	wait_ms(500);
 	
@@ -208,21 +369,128 @@ int main(void)
 	PORTD |= _BV(2);		 // RSTをHにします。リセット解除
 	wait_ms(10);
 	
-	// デバイスの初期化
-	init_lcd();
+	lcdInit();
 	
-	// １行目の表示
-	i2c_puts("Dual Power");
+	//LCD初期表示
+	lcdSetPos(0, VOLTAGE_Y);	lcdPutStr("Voltage @@");
+	lcdSetPos(0, CURRENT_Y);	lcdPutStr("Current @@");
+	wait_sec(3);
 	
-	// ２行目にカーソルを移動
-	i2c_cmd(0b11000000);	// ADDR=0x40
+	//ADC動作設定
+	ADCSRA = 0b10000110		//bit2-0: 101 = 64分周	8MHz/64=125kHz (50k～200kHzであること)
+		| (1 << ADIE);		//割り込み許可（スリープ対応）
 	
-	// カナの表示（このソースプログラムはSJISで記述されていないとうまく表示されません）
-	i2c_puts("VA Meter 2");
+	//ADCの基準電圧と入力ピンを設定する
+	ADMUX =	ADC_SET_PVOLTAGE;
+
+/*	
+	//A/D変換ノイズ低減のスリープモードを設定する
+	set_sleep_mode(SLEEP_MODE_ADC);
+	sei();			//割り込み許可
+	sleep_mode();	//A/Dコンバータ初期化として初回変換開始…変換中…変換完了
+	adcValue = ADC;	//値の読み捨て
+*/
+
+	lcdPutVoltage(15000, 0);
+	lcdPutCurrent(5000, 0);
+	lcdPutVoltage(15000, 1);
+	lcdPutCurrent(5000, 1);
+	
+	idx = 0;
+	idx2 = 0;
 	
     while(1)
     {
-        //TODO:: Please write your application code 
+#if 0
+        //===== 正電源         ==============================================================================
+		//
 		
+		//===== 電圧を測定する =====
+		ADMUX =	ADC_SET_PVOLTAGE;
+		wait_ms(5);	//安定待ち
+		sleep_mode();	//スリープモード突入…変換中…変換完了
+			
+		adcPVoltages[idx] = ADC;
+		adcValue = total(adcPVoltages);
+			
+		//A/D変換値から電圧を求める
+		fv = ((float)adcValue / BUFSIZE) * (F_VREF * 1000) / 1024;	//mV単位
+		fv *= (F_VMETER_RA + F_VMETER_RB) / F_VMETER_RA;	//分圧している値から本来の電圧を求める
+		//ここでfv=F_VREFとした値が測定可能な上限値である
+		//補正
+		//テスターでの実測値と比較して、計算値はF_VREFを中心に約1％ずれていたので補正する。
+		//・計算値がF_VREFより大きいとき、その値は実測値より約1％大きかった。
+		//・計算値がF_VREFより小さいとき、その値は実測値より約1％小さかった。
+		//fv -= (fv - F_VREF * 1000) * 0.01;	//fvはmV単位であることに注意
+			
+		//電圧を表示する
+		mVoltage = (uint16_t)fv;
+		lcdPutVoltage(mVoltage, 0);
+					
+		//===== 電流を測定する =====
+		ADMUX =	ADC_SET_PCURRENT;
+		wait_ms(5);	//安定待ち
+		sleep_mode();	//スリープモード突入…変換中…変換完了
+			
+		adcPCurrents[idx] = ADC;
+		adcValue = total(adcPCurrents);
+			
+		//A/D変換値から電圧を求め、電流を算出する
+		fv = ((float)adcValue / BUFSIZE) * (F_VREF * 1000) / 1024;	//mV単位
+		fa = fv / F_SHUNT_R;	//mA単位
+			
+		//電流を表示する
+		mCurrent = (uint16_t)(fa * 10.0);	//0.1mA単位
+		lcdPutCurrent(mCurrent, 0);
+			
+		//次のループの準備
+		if (++idx == BUFSIZE) idx = 0;
+			
+		//===== 負電源         =============================================================================
+		//
+		
+		//===== 電圧を測定する =====
+		ADMUX =	ADC_SET_NVOLTAGE;
+		wait_ms(5);	//安定待ち
+		sleep_mode();	//スリープモード突入…変換中…変換完了
+			
+		adcNVoltages[idx2] = ADC;
+		adcValue = total(adcNVoltages);
+			
+		//A/D変換値から電圧を求める
+		fv = ((float)adcValue / BUFSIZE) * (F_VREF * 1000) / 1024;	//mV単位
+		fv *= (F_VMETER_RA + F_VMETER_RB) / F_VMETER_RA;	//分圧している値から本来の電圧を求める
+		//ここでfv=F_VREFとした値が測定可能な上限値である
+		//補正
+		//テスターでの実測値と比較して、計算値はF_VREFを中心に約1％ずれていたので補正する。
+		//・計算値がF_VREFより大きいとき、その値は実測値より約1％大きかった。
+		//・計算値がF_VREFより小さいとき、その値は実測値より約1％小さかった。
+		//fv -= (fv - F_VREF * 1000) * 0.01;	//fvはmV単位であることに注意
+			
+		//電圧を表示する
+		mVoltage = (uint16_t)fv;
+		lcdPutVoltage(mVoltage, 1);
+			
+		//===== 電流を測定する =====
+		ADMUX =	ADC_SET_NCURRENT;
+		wait_ms(5);	//安定待ち
+		sleep_mode();	//スリープモード突入…変換中…変換完了
+			
+		adcNCurrents[idx2] = ADC;
+		adcValue = total(adcNCurrents);
+			
+		//A/D変換値から電圧を求め、電流を算出する
+		fv = ((float)adcValue / BUFSIZE) * (F_VREF * 1000) / 1024;	//mV単位
+		fa = fv / F_SHUNT_R;	//mA単位
+			
+		//電流を表示する
+		mCurrent = (uint16_t)(fa * 10.0);	//0.1mA単位
+		lcdPutCurrent(mCurrent, 1);
+			
+		//次のループの準備
+		if (++idx2 == BUFSIZE) idx2 = 0;
+#endif
     }
+	
+	return 0;
 }
